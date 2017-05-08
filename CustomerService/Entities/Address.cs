a2b984191a234d69a9e16ab8038a8429df6a0960
Piotr Polański <polanski.piotr@me.com>
2017-05-08 14:04:56 +0200

    encapsulate Address and Customer, add some validation

--- CustomerService/Entities/Address.cs
+++ CustomerService/Entities/Address.cs
@@ -1,10 +1,28 @@
-﻿namespace CustomerService.Entities
+﻿using System.IO;
+using SimpleValidator.Extensions;
+
+namespace CustomerService.Entities
 {
 	public class Address
 	{
-		public string StreetName { get; set; }
-		public string City { get; set; }
-		public string ZipCode { get; set; }
-		public string Country { get; set; }
+		private Address(){}
+
+		public Address(string streetName, string city, string zipCode, string country)
+		{
+			if(streetName.IsNullOrEmpty()) throw new InvalidDataException("StreetName should not be null or empty");
+			if(city.IsNullOrEmpty()) throw new InvalidDataException("city should not be null or empty");
+			if(zipCode.IsNullOrEmpty()) throw new InvalidDataException("zipCode should not be null or empty");
+			if(country.IsNullOrEmpty()) throw new InvalidDataException("country should not be null or empty");
+
+			StreetName = streetName;
+			City = city;
+			ZipCode = zipCode;
+			Country = country;
+		}
+
+		public string StreetName { get; private set; }
+		public string City { get; private set; }
+		public string ZipCode { get; private set; }
+		public string Country { get; private set; }
 	}
 }
--- CustomerService/Entities/Customer.cs
+++ CustomerService/Entities/Customer.cs
@@ -1,11 +1,27 @@
-﻿namespace CustomerService.Entities
+﻿using System.IO;
+using SimpleValidator.Extensions;
+
+namespace CustomerService.Entities
 {
 	public class Customer
 	{
-		public string Name { get; set; }
-		public string Surname { get; set; }
-		public string TelephoneNumber { get; set; }
-		public Address Address { get; set; }
+		private Customer(){}
+		public Customer(string name, string surname, string telephoneNumber, Address address)
+		{
+			if(name.IsNullOrEmpty()) throw new InvalidDataException("Name should not be emtpy or null");
+			if(surname.IsNullOrEmpty()) throw new InvalidDataException("Surname should not be emtpy or null");
+			if(telephoneNumber.IsNullOrEmpty()) throw new InvalidDataException("TelephoneNumber should not be emtpy or null");
+			if(address.IsNull()) throw new InvalidDataException("Address should not be null");
+
+			Name = name;
+			Surname = surname;
+			TelephoneNumber = telephoneNumber;
+			Address = address;
+		}
+		public string Name { get; private set; }
+		public string Surname { get; private set; }
+		public string TelephoneNumber { get; private set; }
+		public Address Address { get; private set; }
 
 	}
 }
